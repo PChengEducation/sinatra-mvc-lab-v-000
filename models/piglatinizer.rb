6a3542afc36705546f2b147a4d5ba80475d281c7
Pan Cheng <pchengeducation@hotmail.com>
2018-08-22 20:29:16 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,30 +1,21 @@
 class PigLatinizer
 
-  attr_accessor :words
 
-  def initialize(words = nil)
-    @words = words
+def piglatinize(word)
+    vowels = %w{a e i o u}
+    word.each_char do |chr|
+    index = word.index(chr)
+    if index != 0 && vowels.include?(chr.downcase)
+      consonants = word.slice!(0..index-1)
+      return word + consonants + "ay"
+    elsif index == 0 && vowels.include?(chr.downcase)
+      return word + "way"
+    end
   end
+end
 
-  def piglatinize(word)
-
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    alpha = ('a'..'z').to_a
-    consonants = alpha - vowels
-
-      if vowels.include?(word.downcase[0])
-        word + 'way'
-      elsif consonants.include?(word.downcase[0]) && consonants.include?(word.downcase[1]) && consonants.include?(word.downcase[2])
-        word[3..-1] + word[0..2] + 'ay'
-      elsif consonants.include?(word.downcase[0]) && consonants.include?(word.downcase[1])
-        word[2..-1] + word[0..1] + 'ay'
-      elsif consonants.include?(word.downcase[0])
-        word[1..-1] + word[0] + 'ay'
-      end
-  end
-
-  def to_pig_latin(sentence)
-    sentence.split(" ").collect {|word| piglatinize(word)}.join(" ")
-  end
+def to_pig_latin(sentence)
+  sentence.split(" ").collect { |word| piglatinize(word) }.join(" ")
+end
 
 end
